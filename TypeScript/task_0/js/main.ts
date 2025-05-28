7c6c50a7131a2c17417823da8261f9388a7da2e6
SaLaam17 <slaamri@yahoo.fr>
2025-05-28 09:43:20 +0200

    task_0/

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -18,4 +18,33 @@ const student2: Student = {
   location: "Pau",
 };
 
-const studentsList = [student1, student2];
+const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement('table');
+const headerRow = document.createElement('tr');
+
+const nameHeader = document.createElement('th');
+nameHeader.textContent = "Firstname";
+
+const locationHeader = document.createElement('th');
+locationHeader.textContent = "Location";
+
+headerRow.appendChild(nameHeader);
+headerRow.appendChild(locationHeader);
+table.appendChild(headerRow);
+
+studentsList.forEach((student) => {
+  const row = document.createElement('tr');
+
+  const firstNameCell = document.createElement('td');
+  firstNameCell.textContent = student.firstName;
+
+  const locationCell = document.createElement('td');
+  locationCell.textContent =student.location;
+
+  row.appendChild(firstNameCell);
+  row.appendChild(locationCell);
+  table.appendChild(row);
+});
+
+document.body.appendChild(table);
